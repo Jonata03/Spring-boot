fd887e705c10ccf2b41a7abf93b01d8c313cd02c
Jonata03 <jonatanery2016@gmail.com>
2024-04-29 14:14:11 -0400

    finalizando o dia

---
+++ jdbc/src/main/java/io/github/jonata03/jdbc/JdbcApplication.java
@@ -0,0 +1,13 @@
+package io.github.jonata03.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApplication.class, args);
+	}
+
+}
