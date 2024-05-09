a6596b2a113b8525194953ebf75f6dd56d3a81ee
Jonata <jonatanery2016@gmail.com>
2024-05-09 00:13:47 -0400

    ;

--- jdbc/src/main/java/io/github/jonata03/jdbc/exception/SenhaInvalidaException.java
+++ jdbc/src/main/java/io/github/jonata03/jdbc/exception/SenhaInvalidaException.java
@@ -2,7 +2,7 @@ package io.github.jonata03.jdbc.exception;
 
 public class SenhaInvalidaException extends RuntimeException  {
     public SenhaInvalidaException(){
-        super("Senha Invalida")
+        super("Senha Invalida");
     }
 
 }
